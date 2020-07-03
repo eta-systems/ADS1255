550c8ab3d05864d8e515a40c8e7bcae05f0da510
Simon Burkhardt <simon.burkhardt.prime@gmail.com>
2020-07-03 16:32:21 +0200

    first version that works pay attention to: - how GPIO Port and Pin are passed (not as pointers)

---
+++ src/ads1255.c
@@ -0,0 +1,371 @@
+/*******************************************************************************
+ * @file        ads1255.h
+ * @brief       C Library for ads1255/ads1256 family of Analog to Digital
+ *              Conterters (ADC)
+ * @details     This file implements the functionalities of the ADC.
+ * @version     1.0
+ * @author      Simon Burkhardt
+ * @date        2020.05.22
+ * @copyright   (c) 2020 eta systems GmbH
+********************************************************************************
+ * @note        Parts of this library are based on Adien Akhmads Arduino Library 
+
+MIT License
+
+Copyright (c) 2016 Adien Akhmad
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <stdint.h>
+#include "ads1255.h"
+
+// #define DEBUG_ADS1255
+
+#ifdef DEBUG_ADS1255
+#include <stdio.h>
+#endif
+
+/**
+  * @brief  resets, initializes and calibrates the ADS125X
+  * @param  *ads pointer to ads handle
+  * @param  drate the datarate of the converter
+	* @param  gain  the gain of the PGA
+	* @param  buffer_en [0 = off, 1 = on]
+  * @return 
+  * @see    Datasheet Fig. 33 SDATAC Command Sequence
+  */
+uint8_t ADS125X_Init(ADS125X_t *ads, SPI_HandleTypeDef *hspi, uint8_t drate, uint8_t gain, uint8_t buffer_en)
+{
+  ads->hspix = hspi;
+  ads->pga = 1 << gain;
+	
+	ADS125X_CS(ads, 1);
+	ADS125X_CMD_Send(ads, ADS125X_CMD_RESET);
+	HAL_Delay(5);
+	ADS125X_CMD_Send(ads, ADS125X_CMD_SDATAC);
+	
+	uint8_t tmp[5]; // buffer
+#ifdef DEBUG_ADS1255
+	ADS125X_Register_Read(ads, ADS125X_REG_STATUS, tmp, 1);
+	printf("STATUS: %#.2x\n", tmp[0]);
+#endif
+	
+	// enable clockout | ADS125X_PGA1
+	ADS125X_Register_Write(ads, ADS125X_REG_ADCON, ADS125X_CLKOUT_1 | gain);  // enable clockout = clkin/1
+#ifdef DEBUG_ADS1255
+	ADS125X_Register_Read(ads, ADS125X_REG_ADCON, tmp, 1);
+	printf("ADCON: %#.2x\n", tmp[0]);
+#endif
+
+  ADS125X_Register_Write(ads, ADS125X_REG_DRATE, drate);
+#ifdef DEBUG_ADS1255
+	ADS125X_Register_Read(ads, ADS125X_REG_DRATE, tmp, 1);
+	printf("DRATE: %#.2x\n", tmp[0]);
+#endif
+
+ADS125X_Register_Write(ads, ADS125X_REG_IO, 0x00); // all GPIOs are outputs (do not leave floating) - D0 is CLKOUT
+#ifdef DEBUG_ADS1255
+	ADS125X_Register_Read(ads, ADS125X_REG_IO, tmp, 1);
+	printf("IO   : %#.2x\n", tmp[0]);
+#endif
+
+	ADS125X_CMD_Send(ads, ADS125X_CMD_SELFCAL);
+	ADS125X_CS(ads, 0);
+  ADS125X_DRDY_Wait(ads);  // wait ADS1256 to settle after self calibration
+	
+	return 0;
+}
+
+/**
+  * @brief  waits for DRDY pin to go low
+  * @param  *ads pointer to ads handle
+  */
+uint8_t ADS125X_DRDY_Wait(ADS125X_t *ads){
+  while(HAL_GPIO_ReadPin(ads->drdyPort, ads->drdyPin) == GPIO_PIN_SET);
+	return 0;
+}
+
+/**
+  * @brief  toggles chip select pin of ADS
+  * @param  *ads pointer to ads handle
+  * @param  on [0 = unselect, 1 = select]
+  */
+uint8_t ADS125X_CS(ADS125X_t *ads, uint8_t on)
+{
+  if(on)
+		HAL_GPIO_WritePin(ads->csPort, ads->csPin, GPIO_PIN_RESET);
+  else
+		HAL_GPIO_WritePin(ads->csPort, ads->csPin, GPIO_PIN_SET);
+	return 0;
+}
+
+/**
+  * @brief  converts an <int32_t> array of 2's complement code to <float> array of voltage
+  * @param  *ads pointer to ads handle
+  * @param  *pCode pointer to input array
+  * @param  *pVolt pointer to output array
+  * @param  size the length of the array
+  * @see    Datasheet Table 16. Ideal Ouput Code vs. Input Signal
+  */
+void ADS125X_ADC_Code2Volt (ADS125X_t *ads, int32_t *pCode, float *pVolt, uint16_t size){
+	for(uint8_t i=0; i<size; i++){
+		// Vin = Code * 2 * (Vrefp - Vrefn) / ( PGA * 0x7FFFFF )
+		if(pCode[i] & 0x800000) pCode[i] |= 0xff000000;  // fix 2's complement
+		// do all calculations in float. don't change the order of factors --> (adsCode/0x7fffff) will always return 0
+		pVolt[i] = ( (float)pCode[i] * (2.0f * ads->vref) ) / ( ads->pga * 8388607.0f );  // 0x7fffff = 8388607.0f
+	}
+}
+
+/**
+  * @brief  read an analog voltage from the currently selected channel
+  * @param  *ads pointer to ads handle
+  * @return <float> voltage value on analog input
+  * @see    Datasheet Fig. 30 RDATA Command Sequence
+  */
+		
+/* LIBRARY MACHEN SO: */
+/* ALI NIX SCHULD: */
+float ADS125X_ADC_ReadVolt (ADS125X_t *ads){
+	uint8_t spiRx[3] = {0,0,0};
+	spiRx[0] = ADS125X_CMD_RDATA;
+	
+	ADS125X_CS(ads, 1);
+	ADS125X_DRDY_Wait(ads);
+	HAL_SPI_Transmit(ads->hspix, spiRx, 1, 10);
+	HAL_Delay(1);
+	HAL_SPI_Receive(ads->hspix, spiRx, 3, 10);
+	ADS125X_CS(ads, 0);
+	
+#ifdef DEBUG_ADS1255
+	printf("RDATA: %#.2x%.2x%.2x\n", spiRx[0], spiRx[1], spiRx[2]);
+#endif
+	
+	// must be signed integer for 2's complement to work
+	int32_t adsCode = (spiRx[0] << 16) | (spiRx[1] << 8) | (spiRx[2]);
+  if(adsCode & 0x800000) adsCode |= 0xff000000;  // fix 2's complement
+	// do all calculations in float. don't change the order of factors --> (adsCode/0x7fffff) will always return 0
+	return ( (float)adsCode * (2.0f * ads->vref) ) / ( ads->pga * 8388607.0f );  // 0x7fffff = 8388607.0f
+}
+
+float ADS125X_Lego(ADS125X_t *ads, uint8_t p_chan, uint8_t n_chan){
+	// uint8_t channels = ((p_chan << 4)&0xF0) | (n_chan & 0x0F);
+  ADS125X_Register_Write(ads, ADS125X_REG_MUX, p_chan | n_chan);
+	HAL_Delay(1);
+  ADS125X_CMD_Send(ads, ADS125X_CMD_SYNC);
+	HAL_Delay(1);
+  ADS125X_CMD_Send(ads, ADS125X_CMD_WAKEUP);
+	HAL_Delay(1);
+#ifdef DEBUG_ADS1255
+	uint8_t tmp = 0;
+	ADS125X_Register_Read(ads, ADS125X_REG_MUX, &tmp, 1);
+	printf("MUX  : %#.2x\n", tmp);
+#endif
+	
+	uint8_t spiRx[3] = {0,0,0};
+	spiRx[0] = ADS125X_CMD_RDATA;
+	ADS125X_CS(ads, 1);
+	ADS125X_DRDY_Wait(ads);
+	HAL_SPI_Transmit(ads->hspix, spiRx, 1, 10);
+	HAL_Delay(1);
+	HAL_SPI_Receive(ads->hspix, spiRx, 3, 10);
+	ADS125X_CS(ads, 0);
+	
+#ifdef DEBUG_ADS1255
+	printf("RDATA: %#.2x%.2x%.2x\n", spiRx[0], spiRx[1], spiRx[2]);
+#endif
+	
+	// must be signed integer for 2's complement to work
+	int32_t adsCode = (spiRx[0] << 16) | (spiRx[1] << 8) | (spiRx[2]);
+  if(adsCode & 0x800000) adsCode |= 0xff000000;  // fix 2's complement
+	// do all calculations in float. don't change the order of factors --> (adsCode/0x7fffff) will always return 0
+	return ( (float)adsCode * (2.0f * ads->vref) ) / ( ads->pga * 8388607.0f );  // 0x7fffff = 8388607.0f
+}
+
+/**
+  * @brief  reads from internal registers
+  * @param  *ads pointer to ads handle
+  * @param  reg  first register to be read
+  * @param  *pData pointer for return data
+  * @param  n number of registers to read
+  * @return 
+  * @see    Datasheet Fig. 34 RREG Command Example
+  */
+uint8_t ADS125X_Register_Read(ADS125X_t *ads, uint8_t reg, uint8_t* pData, uint8_t n)
+{
+  uint8_t spiTx[2];
+	spiTx[0] = ADS125X_CMD_RREG | reg; // 1st command byte
+	spiTx[1] = n-1;                    // 2nd command byte = bytes to be read -1
+	
+  ADS125X_CS(ads, 1);
+  ADS125X_DRDY_Wait(ads);
+  HAL_SPI_Transmit(ads->hspix, spiTx, 2, 1);
+  HAL_Delay(1); // t6 delay (50*tCLKIN)
+  HAL_SPI_Receive(ads->hspix, pData, n, 1);
+  HAL_Delay(1); // t11 delay
+  ADS125X_CS(ads, 0);
+	return 0;
+}
+
+/**
+  * @brief  writes to internal registers
+  * @param  *ads pointer to ads handle
+  * @param  reg  first register to write to
+  * @param  payload to write to register
+  * @return 
+  * @see    Datasheet Fig. 35 WREG Command Example
+  */
+uint8_t ADS125X_Register_Write(ADS125X_t *ads, uint8_t reg, uint8_t data)
+{
+	uint8_t spiTx[3];
+	spiTx[0] = ADS125X_CMD_WREG | reg; // 1st command byte
+	spiTx[1] = 0;                      // 2nd command byte = payload length = 1 bytes -1 = 0
+	spiTx[2] = data;
+	
+  ADS125X_CS(ads, 1);
+	ADS125X_DRDY_Wait(ads);
+	HAL_SPI_Transmit(ads->hspix, spiTx, 3, 10);
+	HAL_Delay(1);
+  ADS125X_CS(ads, 0);
+	return 0;
+}
+
+/**
+  * @brief  send a single command to the ADS
+  * @param  *ads pointer to ads handle
+  * @param  cmd the command
+  * @return 
+  * @see    Datasheet Fig. 33 SDATAC Command Sequence
+  */
+uint8_t ADS125X_CMD_Send(ADS125X_t *ads, uint8_t cmd)
+{
+  uint8_t spiTx = cmd;
+	
+  ADS125X_CS(ads, 1);
+  ADS125X_DRDY_Wait(ads);
+  HAL_SPI_Transmit(ads->hspix, &spiTx, 1, 1);
+  ADS125X_CS(ads, 0);
+	return 0;
+}
+
+/**
+  * @brief  set internal multiplexer to single channel with AINCOM as negative input
+  * @param  *ads pointer to ads handle
+  * @param  p_chan positive analog input
+  * @see    Datasheet p. 31 MUX : Input Multiplexer Control Register (Address 01h)
+  */
+void ADS125X_Channel_Set(ADS125X_t *ads, int8_t channel)
+{ 
+  ADS125X_ChannelDiff_Set(ads, channel, ADS125X_MUXN_AINCOM); 
+}
+
+/**
+  * @brief  set internal multiplexer to differential input channel
+  * @param  *ads pointer to ads handle
+  * @param  p_chan positive analog input
+  * @param  n_chan negative analog input
+  * @see    Datasheet p. 31 MUX : Input Multiplexer Control Register (Address 01h)
+  */
+uint8_t ADS125X_ChannelDiff_Set(ADS125X_t *ads, int8_t p_chan, int8_t n_chan)
+{
+  // uint8_t channels = ((p_chan << 4)&0xF0) | (n_chan & 0x0F);
+
+  ADS125X_Register_Write(ads, ADS125X_REG_MUX, p_chan | n_chan);
+  ADS125X_CMD_Send(ads, ADS125X_CMD_SYNC);
+  ADS125X_CMD_Send(ads, ADS125X_CMD_WAKEUP);
+#ifdef DEBUG_ADS1255
+	uint8_t tmp = 0;
+	ADS125X_Register_Read(ads, ADS125X_REG_MUX, &tmp, 1);
+	printf("MUX  : %#.2x\n", tmp);
+#endif
+  // ADS125X_CMD_Send(ads, ADS125X_CMD_SYNC);
+  // ADS125X_CMD_Send(ads, ADS125X_CMD_WAKEUP);
+  return 0;
+}
+
+
+/*
+// Call this ONLY after RDATA command
+uint32_t ADS125X_read_uint24(ADS125X_t *ads) {
+  uint8_t _highByte, _midByte, _lowByte;
+  uint32_t value;
+  uint8_t ds = ADS125X_CMD_WAKEUP;
+  HAL_SPI_TransmitReceive(ads->hspix, &ds, &_highByte, 1, 1);
+  HAL_SPI_TransmitReceive(ads->hspix, &ds, &_midByte, 1, 1);
+  HAL_SPI_TransmitReceive(ads->hspix, &ds, &_lowByte, 1, 1);
+
+  // Combine all 3-bytes to 24-bit data using byte shifting.
+  value = ((uint32_t)_highByte << 16) + ((uint32_t)_midByte << 8) + ((uint32_t)_lowByte);
+  return value;
+}
+
+// Call this ONLY after RDATA command
+uint32_t ADS125X_read_int32(ADS125X_t *ads) {
+  uint32_t value = ADS125X_read_uint24(ads);
+
+  if (value & 0x00800000) {
+    value |= 0xff000000;
+  }
+
+  return value;
+}
+
+// Call this ONLY after RDATA command
+float ADS125X_read_float32(ADS125X_t *ads) {
+  uint32_t value = ADS125X_read_int32(ads);
+  return (float)value;
+}
+
+
+float ADS125X_Read_Channel(ADS125X_t *ads) {
+  ADS125X_CS(ads, 1);
+  uint8_t ds = ADS125X_CMD_RDATA;
+  HAL_SPI_Transmit(ads->hspix, &ds, 1, 1);
+  HAL_Delay(1);  // t6
+  float adsCode = ADS125X_read_float32(ads);
+  ADS125X_CS(ads, 0);
+  return ((adsCode / 0x7FFFFF) * ((2 * ads->vref) / (float)ads->pga)) * ads->convFactor;
+}
+
+uint8_t ADS125X_ChannelDiff_Set(ADS125X_t *ads, int8_t p_chan, int8_t n_chan)
+{
+	// read back
+	uint8_t spiDat[5];
+	
+	spiDat[0] = ADS125X_CMD_WREG | ADS125X_REG_MUX;	
+	spiDat[1] = 1 -1;  // payload length = 1 bytes -1
+	//spiDat[2] = ADS125X_MUXP_AIN0 | ADS125X_MUXN_AIN1;
+	spiDat[2] = 
+	while(HAL_GPIO_ReadPin(SPI2_DRDY_GPIO_Port, SPI2_DRDY_Pin) == GPIO_PIN_SET);  // wait for DRDY to go low
+	HAL_SPI_Transmit(&hspi2, spiDat, 3, 10);
+	HAL_Delay(1);
+	// read back
+	spiDat[0] = ADS125X_CMD_RREG | ADS125X_REG_MUX;
+	spiDat[1] = 1 -1; // read 1 bytes
+	while(HAL_GPIO_ReadPin(SPI2_DRDY_GPIO_Port, SPI2_DRDY_Pin) == GPIO_PIN_SET);  // wait for DRDY to go low
+	HAL_SPI_Transmit(&hspi2, spiDat, 2, 10);
+	HAL_Delay(1);
+	HAL_SPI_Receive(&hspi2, spiRx, 1, 10);
+	printf("MUX  : %#.2x\n", spiRx[0]);
+}
+*/
+
+
+
+
---
+++ src/ads1255.h
@@ -0,0 +1,220 @@
+/*******************************************************************************
+ * @file        ads1255.h
+ * @brief       C Library for ads1255/ads1256 family of Analog to Digital
+ *              Conterters (ADC)
+ * @details     This file implements the functionalities of the ADC.
+ * @version     1.0
+ * @author      Simon Burkhardt
+ * @date        2020.05.22
+ * @copyright   (c) 2020 eta systems GmbH
+ * @see         https://github.com/adienakhmad/ADS1256
+********************************************************************************
+ * @note        Parts of this library are based on Adien Akhmads Arduino Library 
+
+MIT License
+
+Copyright (c) 2016 Adien Akhmad
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef ADS1255_H
+#define	ADS1255_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define STM32F4
+
+#if defined(STM32F1)
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_spi.h"
+#elif defined(STM32F4)
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_spi.h"
+#elif defined(STM32L4)
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal_spi.h"
+#elif defined(STM32F3)
+#include "stm32f3xx_hal.h"
+#include "stm32f3xx_hal_spi.h"
+#elif defined(STM32F7)
+#include "stm32f7xx_hal.h"
+#include "stm32f7xx_hal_spi.h"
+#endif
+
+// reference voltage
+#define ADS125X_VREF (2.5f)
+#define ADS125X_OSC_FREQ (7680000)
+
+/*
+const defaults = {
+      clkinFrequency: 7680000,
+      spiFrequency: 976563,
+      spiMode: 1,
+      vRef: 2.5,
+    };
+*/
+
+// ADS1256 Register
+#define ADS125X_REG_STATUS 0x00
+#define ADS125X_REG_MUX    0x01
+#define ADS125X_REG_ADCON  0x02
+#define ADS125X_REG_DRATE  0x03
+#define ADS125X_REG_IO     0x04
+#define ADS125X_REG_OFC0   0x05
+#define ADS125X_REG_OFC1   0x06
+#define ADS125X_REG_OFC2   0x07
+#define ADS125X_REG_FSC0   0x08
+#define ADS125X_REG_FSC1   0x09
+#define ADS125X_REG_FSC2   0x0A
+
+// ADS1256 Command
+// Datasheet p. 34 / Table 24
+// All of the commands are stand-alone
+// except for the register reads and writes (RREG, WREG) 
+// which require a second command byte plus data
+
+#define ADS125X_CMD_WAKEUP   0x00
+#define ADS125X_CMD_RDATA    0x01
+#define ADS125X_CMD_RDATAC   0x03
+#define ADS125X_CMD_SDATAC   0x0f
+#define ADS125X_CMD_RREG     0x10
+#define ADS125X_CMD_WREG     0x50
+#define ADS125X_CMD_SELFCAL  0xF0
+#define ADS125X_CMD_SELFOCAL 0xF1
+#define ADS125X_CMD_SELFGCAL 0xF2
+#define ADS125X_CMD_SYSOCAL  0xF3
+#define ADS125X_CMD_SYSGCAL  0xF4
+#define ADS125X_CMD_SYNC     0xFC
+#define ADS125X_CMD_STANDBY  0xFD
+#define ADS125X_CMD_RESET    0xFE
+
+#define ADS125X_BUFON   0x02
+#define ADS125X_BUFOFF  0x00
+
+#define ADS125X_CLKOUT_OFF     0x00
+#define ADS125X_CLKOUT_1       0x20
+#define ADS125X_CLKOUT_HALF    0x40
+#define ADS125X_CLKOUT_QUARTER 0x60
+
+#define ADS125X_RDATA     0x01    /* Read Data */
+#define ADS125X_RDATAC    0x03    /* Read Data Continuously */
+#define ADS125X_SDATAC    0x0F    /* Stop Read Data Continuously */
+#define ADS125X_RREG      0x10    /* Read from REG */
+#define ADS125X_WREG      0x50    /* Write to REG */
+#define ADS125X_SELFCAL   0xF0    /* Offset and Gain Self-Calibration */
+#define ADS125X_SELFOCAL  0xF1    /* Offset Self-Calibration */
+#define ADS125X_SELFGCAL  0xF2    /* Gain Self-Calibration */
+#define ADS125X_SYSOCAL   0xF3    /* System Offset Calibration */
+#define ADS125X_SYSGCAL   0xF4    /* System Gain Calibration */
+#define ADS125X_SYNC      0xFC    /* Synchronize the A/D Conversion */
+#define ADS125X_STANDBY   0xFD    /* Begin Standby Mode */
+#define ADS125X_RESET     0xFE    /* Reset to Power-Up Values */
+#define ADS125X_WAKEUP    0xFF    /* Completes SYNC and Exits Standby Mode */
+
+// multiplexer codes
+#define ADS125X_MUXP_AIN0 0x00
+#define ADS125X_MUXP_AIN1 0x10
+#define ADS125X_MUXP_AIN2 0x20
+#define ADS125X_MUXP_AIN3 0x30
+#define ADS125X_MUXP_AIN4 0x40
+#define ADS125X_MUXP_AIN5 0x50
+#define ADS125X_MUXP_AIN6 0x60
+#define ADS125X_MUXP_AIN7 0x70
+#define ADS125X_MUXP_AINCOM 0x80
+
+#define ADS125X_MUXN_AIN0 0x00
+#define ADS125X_MUXN_AIN1 0x01
+#define ADS125X_MUXN_AIN2 0x02
+#define ADS125X_MUXN_AIN3 0x03
+#define ADS125X_MUXN_AIN4 0x04
+#define ADS125X_MUXN_AIN5 0x05
+#define ADS125X_MUXN_AIN6 0x06
+#define ADS125X_MUXN_AIN7 0x07
+#define ADS125X_MUXN_AINCOM 0x08
+
+// gain codes
+#define ADS125X_PGA1    0x00
+#define ADS125X_PGA2    0x01
+#define ADS125X_PGA4    0x02
+#define ADS125X_PGA8    0x03
+#define ADS125X_PGA16   0x04
+#define ADS125X_PGA32   0x05
+#define ADS125X_PGA64   0x06
+
+// data rate codes
+/** @note: Data Rate vary depending on crystal frequency. 
+  * Data rates listed below assumes the crystal frequency is 7.68Mhz
+  * for other frequency consult the datasheet.
+  */
+#define ADS125X_DRATE_30000SPS 0xF0
+#define ADS125X_DRATE_15000SPS 0xE0
+#define ADS125X_DRATE_7500SPS 0xD0
+#define ADS125X_DRATE_3750SPS 0xC0
+#define ADS125X_DRATE_2000SPS 0xB0
+#define ADS125X_DRATE_1000SPS 0xA1
+#define ADS125X_DRATE_500SPS 0x92
+#define ADS125X_DRATE_100SPS 0x82
+#define ADS125X_DRATE_60SPS 0x72
+#define ADS125X_DRATE_50SPS 0x63
+#define ADS125X_DRATE_30SPS 0x53
+#define ADS125X_DRATE_25SPS 0x43
+#define ADS125X_DRATE_15SPS 0x33
+#define ADS125X_DRATE_10SPS 0x23
+#define ADS125X_DRATE_5SPS 0x13
+#define ADS125X_DRATE_2_5SPS 0x03
+
+// Struct "Object"
+typedef struct {
+    SPI_HandleTypeDef *hspix;
+    float vref;
+    uint8_t pga;
+    float convFactor;
+    uint32_t oscFreq;
+    GPIO_TypeDef *csPort;
+    uint16_t     csPin;
+    GPIO_TypeDef *drdyPort;
+    uint16_t     drdyPin;
+} ADS125X_t;
+
+uint8_t  ADS125X_CS              (ADS125X_t *ads, uint8_t on);
+uint8_t  ADS125X_DRDY_Wait       (ADS125X_t *ads );
+uint8_t  ADS125X_Init            (ADS125X_t *ads, SPI_HandleTypeDef *hspi, uint8_t drate, uint8_t gain, uint8_t buffer_en);
+uint8_t  ADS125X_Register_Read   (ADS125X_t *ads, uint8_t reg, uint8_t* pData, uint8_t n);
+uint8_t  ADS125X_Register_Write  (ADS125X_t *ads, uint8_t reg, uint8_t data);
+uint8_t  ADS125X_CMD_Send        (ADS125X_t *ads, uint8_t cmd);
+void     ADS125X_ADC_Code2Volt   (ADS125X_t *ads, int32_t *pCode, float *pVolt, uint16_t size);
+float    ADS125X_ADC_ReadVolt    (ADS125X_t *ads);
+float ADS125X_Lego(ADS125X_t *ads, uint8_t p_chan, uint8_t n_chan);
+
+void     ADS125X_Channel_Set     (ADS125X_t *ads, int8_t chan);
+uint8_t  ADS125X_ChannelDiff_Set (ADS125X_t *ads, int8_t p_chan, int8_t n_chan);
+// float    ADS125X_Read_Channel    (ADS125X_t *ads );
+
+uint8_t  ADS125X_Delay_Cycles    (ADS125X_t *ads, uint32_t cycles);
+uint32_t ADS125X_read_uint24     (ADS125X_t *ads );
+uint32_t ADS125X_read_uint32     (ADS125X_t *ads );
+float    ADS125X_read_float32    (ADS125X_t *ads );
+
+
+
+#endif	/* ADS1255_H */
+
